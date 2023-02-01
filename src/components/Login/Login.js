9213463e6a3e551a6c2250fc9cc807e86082023f
Petros Tsakonas <petros-tsakonas@hotmail.com>
2023-02-01 21:48:08 +0200

    Using the useEffect Cleanup Function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,10 +12,19 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    console.log('Checking form validity!');
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log('Cleanup');
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
+
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
